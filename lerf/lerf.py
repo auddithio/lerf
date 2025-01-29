be9a869af5aa3fb6f191c305919b9b7537931e36
auddithio nag <58368314+auddithio@users.noreply.github.com>
2025-01-29 14:41:29 -0800

    Update lerf.py
    changed more types of hash grids

--- lerf/lerf.py
+++ lerf/lerf.py
@@ -31,8 +31,8 @@ class LERFModelConfig(NerfactoModelConfig):
     """maximum scale used to compute relevancy with"""
     num_lerf_samples: int = 24
     hashgrid_layers: Tuple[int, int] = (12, 12)
-    hashgrid_resolutions: Tuple[Tuple[int]] = ((16, 128), (128, 512))
-    hashgrid_sizes: Tuple[int] = (19, 19)
+    hashgrid_resolutions: Tuple[Tuple[int, int], Tuple[int, int]] = ((16, 128), (128, 512))
+    hashgrid_sizes: Tuple[int, int] = (19, 19)
 
 
 class LERFModel(NerfactoModel):
