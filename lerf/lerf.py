90c5e10299180099ed2857100210dc18a8ab6657
auddithio nag <58368314+auddithio@users.noreply.github.com>
2025-01-29 14:48:47 -0800

    Update lerf.py
    made hash_layers any number of int tuple

--- lerf/lerf.py
+++ lerf/lerf.py
@@ -30,7 +30,7 @@ class LERFModelConfig(NerfactoModelConfig):
     max_scale: float = 1.5
     """maximum scale used to compute relevancy with"""
     num_lerf_samples: int = 24
-    hashgrid_layers: Tuple[int, int] = (12, 12)
+    hashgrid_layers: Tuple[int, ...] = (12, 12)
     hashgrid_resolutions: Tuple[Tuple[int, int], Tuple[int, int]] = ((16, 128), (128, 512))
     hashgrid_sizes: Tuple[int, int] = (19, 19)
 
